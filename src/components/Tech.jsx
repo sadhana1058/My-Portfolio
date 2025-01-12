ca1f2bf2fb96c7670ea0bcfbb13edc41cbf9bee1
sadhana1058 <sadhana1058@gmail.com>
2025-01-12 16:45:22 -0500

    tech part

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { RiReactjsLine } from 'react-icons/ri'
+import { SiPython } from "react-icons/si";
+import { FaJava } from "react-icons/fa6";
+import { BiLogoMongodb } from "react-icons/bi";
+import { TbBrandMongodb } from "react-icons/tb";
+import { DiMysql } from "react-icons/di";
 
 const Tech = () => {
   return (
@@ -8,6 +13,18 @@ const Tech = () => {
         <div className='flex flex-wrap items-center justify-center gap-4'>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
               <RiReactjsLine className='text-8xl text-cyan-400'/>  
+            </div>
+            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+              <DiMysql className='text-8xl text-cyan-400'/>  
+            </div>
+            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <TbBrandMongodb   className='text-8xl  #16a34a'/>  
+            </div>
+            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+              <SiPython  className='text-8xl text-cyan-400'/>  
+            </div>
+            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            <FaJava  className='text-8xl text-cyan-400'/>  
             </div>
         </div>
     </div>
